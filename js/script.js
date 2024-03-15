ea164b7c97d908f68dfb43f0a2d8ff884064f72b
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-15 13:32:16 +0100

    created grid with square with js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // <div class="square-easy"></div>
+
+function generateNewElementGrid(content){
+    // creo un elemento del tipo <div>...</div>
+    const newElement = document.createElement('div');
+    newElement.classList.add('square-easy');
+    return newElement;
+}
+
+// creo una variabile per assegnare dove mettere i quadrati
+let gridElement = document.getElementById('grid');
+//creo ciclo for per inserire i quadrati nella grid
+for (let i = 0; i < 100; i++) {
+    let newSquare = generateNewElementGrid()
+    // aggiungo quadrato alla grid
+    gridElement.appendChild(newSquare)
+}
