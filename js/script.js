1d549e82418e549681cf8756de51bfa7dc8830ee
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-15 17:26:04 +0100

    exercise completed with bonus

--- js/script.js
+++ js/script.js
@@ -1,44 +1,83 @@
 // <div class="square-easy"></div>
-
 function generateNewElementGrid(content){
     // creo un elemento del tipo <div>...</div>
-    const newElement = document.createElement('div');
+    let newElement = document.createElement('div');
     newElement.classList.add('square-easy');
     return newElement;
-    if (x.style.display === "none") {
-        x.style.display = "block";
-      } else {
-        x.style.display = "none";
-      }
 }
 
 let play = document.getElementById('play');
 play.addEventListener('click', function(){
-    
     // genero la griglia in base alla difficoltà
-    let difficulty = document.getElementById('difficulty');
+    let difficulty = document.getElementById('difficulty').value;
     let grid = document.getElementById('grid');
     let rBtnN = document.getElementById('reset-btn')
-    grid.classList.toggle('d-block');   
-    rBtnN.classList.toggle('d-flex');
-
+    grid.classList.add('d-block');   
+    rBtnN.classList.add('d-flex');
     let gridElement = document.getElementById('grid');
     //creo ciclo for per inserire i quadrati nella grid
-    for (let i = 0; i < 100; i++) {
-        let newSquare = generateNewElementGrid()
-        // aggiungo quadrato alla grid
-        gridElement.appendChild(newSquare);
-        // coloro di azzurro quando clicco il quadrato
-        newSquare.addEventListener('click', function(){
-            newSquare.classList.add('square-click')
-            console.log(`Hai selezionato la casella numero: ${i + 1}`);    
-        })
-
-        let resetButton = document.getElementById('reset');
-        resetButton.addEventListener('click', function(){
-            newSquare.classList.remove('square-click');
-            console.log('Gioco Ricominciato')
-        })
+    if(difficulty == 'hard'){
+        for (let i = 0; i < 49; i++) {
+            let newSquare = generateNewElementGrid()
+            newSquare.classList.add('hard')
+            // aggiungo quadrato alla grid
+            gridElement.appendChild(newSquare);
+            console.log('ok')
+            // coloro di azzurro quando clicco il quadrato
+            newSquare.addEventListener('click', function(){
+                newSquare.classList.add('square-click')
+                console.log(`Hai selezionato la casella numero: ${i + 1}`);    
+            })
+    
+            let resetButton = document.getElementById('reset');
+            resetButton.addEventListener('click', function(){
+                newSquare.classList.remove('square-click');
+                console.log('Gioco Ricominciato')
+                let container = document.getElementById('container')
+                container.innerHTML = ""
+            })
+        }
+    } else if(difficulty == 'medium'){
+        for (let i = 0; i < 81; i++) {
+            let newSquare = generateNewElementGrid()
+            newSquare.classList.add('medium')
+            // aggiungo quadrato alla grid
+            gridElement.appendChild(newSquare);
+            console.log('ok')
+            // coloro di azzurro quando clicco il quadrato
+            newSquare.addEventListener('click', function(){
+                newSquare.classList.add('square-click')
+                console.log(`Hai selezionato la casella numero: ${i + 1}`);    
+            })
+    
+            let resetButton = document.getElementById('reset');
+            resetButton.addEventListener('click', function(){
+                newSquare.classList.remove('square-click');
+                console.log('Gioco Ricominciato')
+                let container = document.getElementById('container')
+                container.innerHTML = ""
+            })
+        }
+    } else {
+        for (let i = 0; i < 100; i++) {
+            let newSquare = generateNewElementGrid()
+            // aggiungo quadrato alla grid
+            gridElement.appendChild(newSquare);
+            console.log('ok')
+            // coloro di azzurro quando clicco il quadrato
+            newSquare.addEventListener('click', function(){
+                newSquare.classList.add('square-click')
+                console.log(`Hai selezionato la casella numero: ${i + 1}`);    
+            })
+    
+            let resetButton = document.getElementById('reset');
+            resetButton.addEventListener('click', function(){
+                newSquare.classList.remove('square-click');
+                console.log('Gioco Ricominciato')
+                let container = document.getElementById('container');
+                container.innerHTML = ''
+            })
+        }
     }
 })
 
